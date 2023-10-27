cfed43e1df5cfc57c0f9d613ae7e3ef3a9d78f75
Harsh Hirani <hiraniharsh7@gmail.com>
2023-10-27 16:04:22 +0530

    60

--- server/checker.php
+++ server/checker.php
@@ -11,25 +11,31 @@ $SQL = "SELECT sum(quantity) as total FROM ".$username."_portfolio WHERE stockId
 $data = mysqli_query($conn,$SQL);
 $status = 'false';
 
+if($opcode == 5){
+    $status = 'true';
+    $load = array() ;
+    $sql0 = "SELECT sum(quantity) as total FROM ".$username."_portfolio WHERE stockId = '".$stock."' and fixed = '0' ";
+    $sql2 = "SELECT sum(quantity) as total FROM ".$username."_portfolio WHERE stockId = '".$stock."' and fixed = '2' ";
+    $d1 = mysqli_query($conn,$sql0);
+    $d2 = mysqli_query($conn,$sql2);
+
+    $dump = mysqli_fetch_assoc($d1);
+    $dump2 = mysqli_fetch_assoc($d2);
+    $load['allowed0'] = (int)$dump['total'];
+    $load['allowed2'] = (int)$dump2['total'];
+    $load['stockId'] = (int)$stock;
+    $load['name'] = $stockarray[$stock];
+    $load['price'] = getValue($stock,$stockarray,$conn);
+
+echo json_encode(["status" => $status,"code"=>$opcode, "data" => $load]);
+}else{
+
+
+
 if ($data) {
     $status = 'true';
     $load = array() ;
-    if($opcode == 5){
-        $sql0 = "SELECT sum(quantity) as total FROM ".$username."_portfolio WHERE stockId = '".$stock."' and fixed = '0' ";
-        $sql2 = "SELECT sum(quantity) as total FROM ".$username."_portfolio WHERE stockId = '".$stock."' and fixed = '2' ";
-        $d1 = mysqli_query($conn,$sql0);
-        $d2 = mysqli_query($conn,$sql2);
-
-        $dump = mysqli_fetch_assoc($d1);
-        $dump2 = mysqli_fetch_assoc($d2);
-        $load['allowed0'] = (int)$dump['total'];
-        $load['allowed2'] = (int)$dump2['total'];
-        $load['stockId'] = (int)$stock;
-        $load['name'] = $stockarray[$stock];
-        $load['price'] = getValue($stock,$stockarray,$conn);
     
-    echo json_encode(["status" => $status,"code"=>$opcode, "data" => $load]);
-    }else{
         $dump = mysqli_fetch_assoc($data);
         
         $load['allowed'] = (int)$dump['total'];
@@ -38,8 +44,9 @@ if ($data) {
         $load['price'] = getValue($stock,$stockarray,$conn);
     
     echo json_encode(["status" => $status,"code"=>$opcode, "data" => $load]);
-    }
+    
 }else{  
     echo json_encode(["status" => $status, "data" => array("error" => mysql_error($conn))]);
+}
 }
 ?>
