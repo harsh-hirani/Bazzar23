3b084de44235fc0ec05e052b891c03a368a1c8b7
Harsh Hirani <hiraniharsh7@gmail.com>
2023-10-28 19:08:34 +0530

    101.12345

--- server/initial.php
+++ server/initial.php
@@ -8,9 +8,11 @@ $balance = $ttmmpp['balance'];
 $load = array();
 $load['balance'] = (float)$balance;
 $load['tax']= (float)$ttmmpp['tax'];
-$sql = "SELECT SUM(quantity*value) as investment,SUM(pal) as pal from `".$username."_portfolio`";
+$sql = "SELECT SUM(pal) as pal from `".$username."_portfolio`";
+$SQL2 = "SELECT SUM(quantity*value) as investment from `".$username."_portfolio` WHERE fixed='0' or fixed='2'";
+$ttmmpp = mysqli_fetch_assoc(mysqli_query($conn,$SQL2));
+$load['investment'] = (float)$ttmmpp['investment'];
 $ttmmpp = mysqli_fetch_assoc(mysqli_query($conn,$sql));
-$load['invenstment'] = (float)$ttmmpp['investment'];
 $load['pal'] = (float)$ttmmpp['pal'];
 echo json_encode(["status" => 'true', "data" => $load]);
 ?>
