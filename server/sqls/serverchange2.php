f7d0ebd329adc784724add29ed7b0c566f1537c3
Harsh Hirani <hiraniharsh7@gmail.com>
2023-10-30 13:52:00 +0530

    s

--- server/sqls/serverchange2.php
+++ server/sqls/serverchange2.php
@@ -1,20 +1,26 @@
 <?php
 include '../conn.php';
 for ($i=1; $i < 35; $i++){
-$sql = "CREATE TABLE `stocklinear_".$stockarray[$i]."` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `datetime` BIGINT(20) NOT NULL , `buyOrSell` INT(2) NOT NULL COMMENT 'buy = 0\r\nsell = 1' , `quantity` INT(200) NOT NULL , `newPrice` FLOAT(50) NOT NULL , `beforePrice` FLOAT(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
-if ((mysqli_query($conn, $sql))) {
-    echo "success - ".$stockarray[$i]." 1<br>";
-}else{
-    echo "error - ".$stockarray[$i]." 1<br>";
-    echo mysqli_error($conn)."<br>";
-}
-$sql = "CREATE TABLE `stock_profile_".$stockarray[$i]."` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `time` BIGINT(20) NOT NULL , `open` FLOAT(11) NOT NULL , `high` FLOAT(11) NOT NULL , `low` FLOAT(11) NOT NULL , `close` FLOAT(11) NOT NULL , `volume` INT(11) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
-if ((mysqli_query($conn, $sql))) {
-    echo "success - ".$stockarray[$i]." 2<br>";
-}else{
-    echo "error - ".$stockarray[$i]." 2<br>";
-    echo mysqli_error($conn)."<br>";
-}
+    try{
+
+        $sql = "CREATE TABLE `stocklinear_".$stockarray[$i]."` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `datetime` BIGINT(20) NOT NULL , `buyOrSell` INT(2) NOT NULL COMMENT 'buy = 0\r\nsell = 1' , `quantity` INT(200) NOT NULL , `newPrice` FLOAT(50) NOT NULL , `beforePrice` FLOAT(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+        if ((mysqli_query($conn, $sql))) {
+            echo "success - ".$stockarray[$i]." 1<br>";
+        }else{
+            echo "error - ".$stockarray[$i]." 1<br>";
+            echo mysqli_error($conn)."<br>";
+        }
+        $sql = "CREATE TABLE `stock_profile_".$stockarray[$i]."` ( `id` INT(255) NOT NULL AUTO_INCREMENT , `time` BIGINT(20) NOT NULL , `open` FLOAT(11) NOT NULL , `high` FLOAT(11) NOT NULL , `low` FLOAT(11) NOT NULL , `close` FLOAT(11) NOT NULL , `volume` INT(11) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+        if ((mysqli_query($conn, $sql))) {
+            echo "success - ".$stockarray[$i]." 2<br>";
+        }else{
+            echo "error - ".$stockarray[$i]." 2<br>";
+            echo mysqli_error($conn)."<br>";
+        }
+    }catch(Exception $e){
+        echo "error - ".$stockarray[$i]." 3<br>";
+        echo $e->getMessage()."<br>";
+    }
 }
 $values = array(
     3350,220,1380,380,600,1470,560,7500,15,
