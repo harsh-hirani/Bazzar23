575161331d3018c4c29297ce450989d18116940d
Harsh Hirani <hiraniharsh7@gmail.com>
2023-10-31 22:21:23 +0530

    131.1155

--- server/sqls/serverchange3.php
+++ server/sqls/serverchange3.php
@@ -13,7 +13,8 @@ try{
         // $sql = "INSERT INTO `stocklinear_".$stockarray[$i]."` (`id`, `datetime`, `buyOrSell`, `quantity`, `newPrice`, `beforePrice`) VALUES (NULL, '1697341129959', '1', '0', '".$values[$i-1]."', '0');";
         // if(mysqli_query($conn,$sql)){
         //     echo "success - ".$stockarray[$i]." 1<br>";}
-        $sql = "INSERT INTO `stock_profile_".$stockarray[$i]."` (`time`, `open`, `high`, `low`, `close`, `volume`) VALUES ('1697341129959', '".$values[$i-1]."', '".($values[$i-1]+2)."','".($values[$i-1]-2)."', '".$values[$i-1]."', '0');";
+        $sql = "DELETE FROM stock_profile_".$stockarray[$i]." WHERE `id` > 6";
+        // $sql = "INSERT INTO `stock_profile_".$stockarray[$i]."` (`time`, `open`, `high`, `low`, `close`, `volume`) VALUES ('1697341129959', '".$values[$i-1]."', '".($values[$i-1]+2)."','".($values[$i-1]-2)."', '".$values[$i-1]."', '0');";
         if(mysqli_query($conn,$sql)){
             echo "success - ".$stockarray[$i]." 1<br>";}
     }
