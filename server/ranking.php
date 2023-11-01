61a8c05157fd8b5d3251b1e5f5cace4c2bdc94e7
Harsh Hirani <hiraniharsh7@gmail.com>
2023-11-02 01:03:52 +0530

    k1

--- server/ranking.php
+++ server/ranking.php
@@ -11,8 +11,8 @@ function getName($id,$c){
     return $value;
 }
 try{
-$sql = "With cte as( SELECT id,(balance+invest) as `param`, RANK() OVER (ORDER BY param DESC) ranking FROM `user_current_sts`) select * from cte";
-// $sql = "SELECT id,balance,(balance+invest) as `param` FROM `user_current_sts` ORDER BY `param` DESC";
+// $sql = "With cte as( SELECT id,(balance+invest) as `param`, RANK() OVER (ORDER BY `param` DESC) ranking FROM `user_current_sts`) select * from cte";
+$sql = "SELECT id,balance,(balance+invest) as `param` FROM `user_current_sts` ORDER BY `param` DESC";
 $res = mysqli_query($conn,$sql);
 $load = array();
 $rows = mysqli_num_rows($res);
@@ -20,7 +20,7 @@ for($i=0;$i<$rows;$i++) {
     $row = mysqli_fetch_assoc($res);
     $load[] = array(
         "id" => $row['id'],
-        "rank" => $row['ranking'],
+        "rank" => $i+1,
         "name" => getName($row['id'],$conn),
         "balance" => $row['param'],
     );
