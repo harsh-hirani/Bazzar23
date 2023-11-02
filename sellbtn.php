097b2c417b65d7780c9e8fc844f51a629c5ae0b7
Harsh Hirani <hiraniharsh7@gmail.com>
2023-11-03 00:17:02 +0530

    test1255.24

--- sellbtn.php
+++ sellbtn.php
@@ -5,7 +5,7 @@
 
 
 <button class="sell" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"
- onclick="loadoffset('Short Sell')"><i class="bi bi-graph-up-arrow"></i>
+ onclick="loadoffset('Short Sell')"><i class="bi bi-graph-down-arrow"></i>
 SS</button>
 <button class="buy" id="ssbtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" 
 onclick="loadoffset('Square OFF')"><i class="bi bi-graph-up-arrow"></i>
