049b4d323cfd9e445916a55c7168d5e6fbe38dbc
Harsh Hirani <hiraniharsh7@gmail.com>
2023-11-02 14:53:57 +0530

    4

--- server/sqls/serverchange3.php
+++ server/sqls/serverchange3.php
@@ -13,17 +13,27 @@ try{
         $sql = "DELETE FROM stock_profile_".$stockarray[$i]." WHERE `id` > 0";
         // $sql = "INSERT INTO `stock_profile_".$stockarray[$i]."` (`time`, `open`, `high`, `low`, `close`, `volume`) VALUES ('1697341129959', '".$values[$i-1]."', '".($values[$i-1]+2)."','".($values[$i-1]-2)."', '".$values[$i-1]."', '0');";
         if(mysqli_query($conn,$sql)){
-            echo "success - ".$stockarray[$i]." 1<br>";}
+            echo "success - ".$stockarray[$i]." 1<br>";
+        }else{
+            echo "error - ".$stockarray[$i]." 1<br>";
+            echo mysqli_error($conn)."<br>";
         }
+        
         $sql = "DELETE FROM stocklinear_".$stockarray[$i]." WHERE `id` > 0";
         if(mysqli_query($conn,$sql)){
             echo "success - ".$stockarray[$i]." 2<br>";
+        }else{
+            echo "error - ".$stockarray[$i]." 1<br>";
+            echo mysqli_error($conn)."<br>";
         }
         $sql = "INSERT INTO `stocklinear_".$stockarray[$i]."` (`id`, `datetime`, `buyOrSell`, `quantity`, `newPrice`, `beforePrice`) VALUES (NULL, '1697341129959', '1', '0', '".$values[$i-1]."', '0');";
         if(mysqli_query($conn,$sql)){
             echo "success - ".$stockarray[$i]." 3<br>";
+        }else{
+            echo "error - ".$stockarray[$i]." 1<br>";
+            echo mysqli_error($conn)."<br>";
         }
-
+    }
 }catch(Exception $e){
     echo "error - ".$stockarray[$i]." 3<br>";
     echo $e->getMessage()."<br>";
